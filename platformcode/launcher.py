162bb218e2b31d73af84d89df2b8e4a8385d511f
Dentaku65 <dentaku65@gmail.com>
2015-09-13 19:39:12 +0200

    small lang update

--- platformcode/launcher.py
+++ platformcode/launcher.py
@@ -154,7 +154,7 @@ def run():
                         else:
                             import xbmcgui
                             ventana_error = xbmcgui.Dialog()
-                            ok = ventana_error.ok ("plugin", "No hay nada para reproducir")
+                            ok = ventana_error.ok ("plugin", "Niente da riprodurre")
                     else:
                         logger.info("pelisalacarta.platformcode.launcher no channel 'play' method, executing core method")
                         xbmctools.play_video(channel=channel_name, server=item.server, url=item.url, category=item.category, title=item.title, thumbnail=item.thumbnail, plot=item.plot, extra=item.extra, subtitle=item.subtitle, video_password = item.password, fulltitle=item.fulltitle, Serie=item.show)
