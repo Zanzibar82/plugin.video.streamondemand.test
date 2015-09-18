cd7540faf7edf911b30f8e2a25a20d60d39855e4
Dentaku65 <dentaku65@gmail.com>
2015-09-18 20:53:01 +0200

    fix

--- servers/megadrive.py
+++ servers/megadrive.py
@@ -1,40 +1,60 @@
 # -*- coding: utf-8 -*-
-#------------------------------------------------------------
+# ------------------------------------------------------------
 # pelisalacarta - XBMC Plugin
 # Conector para megadrive
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
-#------------------------------------------------------------
+# by DrZ3r0
+# ------------------------------------------------------------
 
-import urlparse,urllib2,urllib,re
-import os
+import re
 
 from core import scrapertools
 from core import logger
-from core import config
 
-def get_video_url( page_url , premium = False , user="" , password="", video_password="" ):
+
+# Returns an array of possible video url's from the page_url
+def get_video_url(page_url, premium=False, user="", password="", video_password=""):
     logger.info("[megadrive.py] get_video_url(page_url='%s')" % page_url)
     video_urls = []
+
+    data = scrapertools.cache_page(page_url)
+
+    data_pack = scrapertools.find_single_match(data, "(eval.function.p,a,c,k,e,.*?)\s*</script>")
+    if data_pack != "":
+        from core import unpackerjs3
+        data_unpack = unpackerjs3.unpackjs(data_pack)
+        if data_unpack == "":
+            from lib.jsbeautifier.unpackers import packer
+            data_unpack = packer.unpack(data_pack)
+        data = data_unpack
+
+    video_url = scrapertools.find_single_match(data, 'file"?\s*:\s*"([^"]+)",')
+    video_urls.append(["[megadrive]", video_url])
+
+    for video_url in video_urls:
+        logger.info("[megadrive.py] %s - %s" % (video_url[0], video_url[1]))
+
     return video_urls
 
-# Encuentra vídeos del servidor en el texto pasado
+
+# Encuentra v�deos del servidor en el texto pasado
 def find_videos(data):
     encontrados = set()
     devuelve = []
 
-    # http://megadrive.tv/embed-u0b9yjknsu8i-800x400.html
-    patronvideos  = '(megadrive.tv/embed-[a-z0-9]+[^.]+.html)'
-    logger.info("[megadrive.py] find_videos #"+patronvideos+"#")
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+    patronvideos = r"""http://megadrive.tv/(?:embed-)?([a-z0-9A-Z]+)"""
+    logger.info("[megadrive.py] find_videos #" + patronvideos + "#")
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
 
     for match in matches:
         titulo = "[megadrive]"
-        url = "http://"+match
+        url = 'http://megadrive.tv/embed-%s-640x360.html' % match
+
         if url not in encontrados:
-            logger.info("  url="+url)
-            devuelve.append( [ titulo , url , 'megadrive' ] )
+            logger.info("  url=" + url)
+            devuelve.append([titulo, url, 'megadrive'])
             encontrados.add(url)
         else:
-            logger.info("  url duplicada="+url)
+            logger.info("  url duplicada=" + url)
 
     return devuelve
